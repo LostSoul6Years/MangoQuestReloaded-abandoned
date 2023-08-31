5afffc23c0b0e7f933d8756f8ded6546a5762a86
LostSoul <jjcdeveloper@gmail.com>
2023-08-31 23:07:37 +0800

    2.9.0 commit

--- src/main/java/me/Cutiemango/MangoQuest/questobject/interfaces/NPCObject.java
+++
@@ -1,9 +0,0 @@
-package me.Cutiemango.MangoQuest.questobject.interfaces;
-
-import net.citizensnpcs.api.npc.NPC;
-
-public interface NPCObject
-{
-	NPC getTargetNPC();
-	void setTargetNPC(NPC targetNPC);
-}
